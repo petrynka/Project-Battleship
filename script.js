aa1da0f5e1dea1ecf17384b56020b9c4011d1242
petrynka <61549290+petrynka@users.noreply.github.com>
2025-04-18 15:05:05 +0300

    Write fab func Player

--- script.js
+++ script.js
@@ -95,3 +95,25 @@ function GameBoard(){
     getBoardState
   }
 }
+
+function Player(name){
+  const isAI = name.toLowerCase() === 'computer';
+
+  const attack = (enemyBoard,x,y) => {
+    enemyBoard.receiveAttack(x,y);
+  }
+
+  const randomAttack = (enemyBoard) => {
+    const x = Math.floor(Math.random() * 10);
+    const y = Math.floor(Math.random() * 10);
+
+    return enemyBoard.receiveAttack(x,y);
+  }
+
+  return{
+    name,
+    isAI,
+    attack,
+    randomAttack
+  }
+}
